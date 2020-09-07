fe114603788a5274a6d033d7834a3ecdfd651402
David Anwar <hapidun10@yahoo.co.id>
2020-09-07 07:56:17 +0700

    add submit event and comunicate between child

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -6,8 +6,9 @@ class SearchBar extends React.Component {
     // }
 
     state = {term: ''}
-    onFormSubmit(event) {
+    onFormSubmit = (event) => {
         event.preventDefault();
+        this.props.onSubmit(this.state.term);
     }
   render() {
     return (
